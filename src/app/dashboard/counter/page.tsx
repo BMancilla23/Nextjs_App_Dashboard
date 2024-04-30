b926618930486e41793266d70972d768f91a28a3
bmancilla23 <bmancilla119@gmail.com>
2024-04-29 22:34:06 -0500

    Página counter creada

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,18 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+import { Metadata } from "next";
+// emmet prc create a page component
+
+export const metadata: Metadata = {
+ title: 'Counter Page',
+ description: 'Counter Simple',
+};
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1>Productos en el carrito</h1>
+      <CartCounter value={10}/>
+    </div>
+  );
+}
